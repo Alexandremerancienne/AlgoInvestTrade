309969a757703758f9f666b88e8c0d61ce4c36bf
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-04-22 16:19:44 +0300

    Timer added to algorithm

--- bruteforce.py
+++ bruteforce.py
@@ -1,6 +1,9 @@
 # Brute force algorithm. Complexity: O(2^n)
 
+import time
+
 def brute_force_search(shares_list, budget, portfolio=[]):
+    start = time.time()
     if shares_list:
         # Option 1 : first share of shares_list excluded from portfolio
         portfolio1, portfolio1_profits = brute_force_search(shares_list[1:], budget, portfolio)
@@ -18,9 +21,9 @@ def brute_force_search(shares_list, budget, portfolio=[]):
         # Option 1 returned as the best option
         return portfolio1, portfolio1_profits
     else:
-        return list(share[0] for share in portfolio), \
-               [float(f"{sum(share[2] for share in portfolio) - sum(share[1] for share in portfolio):.2f}"),
-                sum(share[1] for share in portfolio)]
+        return [share[0] for share in portfolio], \
+               (float(f"{sum(share[2] for share in portfolio) - sum(share[1] for share in portfolio):.2f}"),
+                sum(share[1] for share in portfolio), start)
 
 
 if __name__ == "__main__":
@@ -39,3 +42,4 @@ if __name__ == "__main__":
     print(f"Brute-force portfolio : {brute_force_portfolio[0]}")
     print(f"Portfolio price : {brute_force_portfolio[1][1]}")
     print(f"Portfolio benefits : {brute_force_portfolio[1][0]}")
+    print(f"Algorithm executed in : {time.time() - brute_force_portfolio[1][2]}s")
