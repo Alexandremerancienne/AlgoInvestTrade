d8b325a120a0d1b9a5af78acbb45382cbb9fca48
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-04-28 16:48:42 +0300

    Comparaison item modified line 18 (if portfolio1_profits[0] < portfolio2_profits[0])

--- bruteforce.py
+++ bruteforce.py
@@ -1,4 +1,4 @@
-# Brute force algorithm. Complexity: O(2^n)
+# Brute force algorithm - Complexity: O(2^n)
 
 import time
 
@@ -15,13 +15,13 @@ def brute_force_search(shares_list, budget, portfolio=[]):
             portfolio2, portfolio2_profits = brute_force_search(shares_list[1:], budget, portfolio + [first_share])
 
             # Option 2 returned as the best option
-            if portfolio1_profits < portfolio2_profits:
+            if portfolio1_profits[0] < portfolio2_profits[0]:
                 return portfolio2, portfolio2_profits
 
         # Option 1 returned as the best option
         return portfolio1, portfolio1_profits
     else:
-        return [share[0] for share in portfolio], \
+        return list(share[0] for share in portfolio), \
                (float(f"{sum(share[2] for share in portfolio) - sum(share[1] for share in portfolio):.2f}"),
                 sum(share[1] for share in portfolio), start)
 
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     # Application of brute force algorithm
 
     brute_force_portfolio = brute_force_search(shares, budget=500, portfolio=[])
-    print(f"Brute-force portfolio : {brute_force_portfolio[0]}")
+    print(f"Brute force portfolio : {brute_force_portfolio[0]}")
     print(f"Portfolio price : {brute_force_portfolio[1][1]}")
     print(f"Portfolio benefits : {brute_force_portfolio[1][0]}")
     print(f"Algorithm executed in : {time.time() - brute_force_portfolio[1][2]}s")
